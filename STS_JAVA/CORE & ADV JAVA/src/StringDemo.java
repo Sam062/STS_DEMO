d2164114b8254e916d60ec9756f5671994f09135
SAM ! <sam062>
2020-01-23 15:48:09 +0530

    after fresh start in jan2020

--- STS_JAVA/CORE & ADV JAVA/src/StringDemo.java
+++ STS_JAVA/CORE & ADV JAVA/src/StringDemo.java
@@ -6,7 +6,9 @@ public class StringDemo {
 		str=str.concat("Mishra");
 		System.out.println(str);
 		
-		
+		StringBuilder sb=new StringBuilder("Hello world");
+		sb.substring(2, 7);
+		sb.append(true);
 		        int result = 0;
 		        short s = 42;
 		        Long x = new Long("42");
@@ -16,11 +18,17 @@ public class StringDemo {
 		        Integer y2 = new Integer("42");
 		        Integer z2 = new Integer(42);
 
+		        StringBuilder sba=new StringBuilder("");
+		        String sa="";
+		        
+		        
+		        
+		        System.out.println(sa.equals(sba.toString()));
 		        if (x == y) /* Line 13 */
 		            result = 1;
 		        if (x.equals(y) ) /* Line 15 */
 		            result = result + 10;
-		        if (x.equals(z) ) /* Line 17 */
+		        if (x.toString().equals(z.toString()) ) /* Line 17 */
 		            result = result + 100;
 		        if (x.equals(x2) ) /* Line 19 */
 		            result = result + 1000;
